d45b68ea30e3b48ac3ce22bbc069d108c27e82df
Dengcheng Yan <yanzhou@mail.ustc.edu.cn>
2014-12-14 15:52:47 +0800

    Initial commit:create scrapy project and add gitignore

---
+++ packagist/settings.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+# Scrapy settings for PackagistScrapy project
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/en/latest/topics/settings.html
+#
+
+BOT_NAME = 'packagist'
+
+SPIDER_MODULES = ['packagist.spiders']
+NEWSPIDER_MODULE = 'packagist.spiders'
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'PackagistScrapy (+http://www.yourdomain.com)'
