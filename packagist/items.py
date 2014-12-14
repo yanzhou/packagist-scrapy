ef74abe5a8557be395357990d5aec272cba9acf2
Dengcheng Yan <yanzhou@mail.ustc.edu.cn>
2014-12-14 17:10:56 +0800

    Declare PackageItem and UserItem

--- packagist/items.py
+++ packagist/items.py
@@ -8,7 +8,43 @@
 import scrapy
 
 
-class PackagistItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+class PackageItem(scrapy.Item):
+    """
+    Package information
+    """
+    # vendor name
+    vendor = scrapy.Field()
+    # package name
+    name = scrapy.Field()
+    # tags, list
+    tags = scrapy.Field()
+    # downloads info, including number of installs of overall, 30-days and today
+    downloads = scrapy.Field()
+    # description
+    description = scrapy.Field()
+    # details info, including urls of maintainers, homepage, canonical, source, issues, IRC and so on
+    details = scrapy.Field()
+    # detailed versions info
+    # including version tag, source-reference, release-data, licence, authors, requires, devRequires, suggests,
+    # provides, conflicts and replaces
+    # type: dict, version tag as the key
+    versions = scrapy.Field()
+
+
+class UserItem(scrapy.Item):
+    """
+    User information
+    """
+    # username
+    username = scrapy.Field()
+    # register date
+    register_date = scrapy.Field()
+    # packages maintained or created by the user
+    # type: dict, "vendor-name/package-name" as the key
+    # including the number of total downloads and stars
+    packages = scrapy.Field()
+    # packages that user stars
+    # url sample: https://packagist.org/users/Seldaek/favorites/
+    # type: dict, "vendor-name/package-name" as the key
+    # including the number of total downloads and stars
+    favorites = scrapy.Field()
