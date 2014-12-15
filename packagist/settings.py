5a055525efb1a01dc92170d7142d100b97a68e20
Dengcheng Yan <yanzhou@mail.ustc.edu.cn>
2014-12-15 21:57:05 +0800

    log and download related settings

--- packagist/settings.py
+++ packagist/settings.py
@@ -16,5 +16,12 @@ ITEM_PIPELINES = {
     'packagist.pipelines.MongoStorePipeline': 300,
 }
 
+CONCURRENT_REQUESTS_PER_DOMAIN = 4
+DOWNLOAD_DELAY = 0.25
+DOWNLOAD_TIMEOUT = 3600
+LOG_ENABLED = True
+LOG_ENCODING = 'utf-8'
+LOG_FILE = 'log.txt'
+
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
 #USER_AGENT = 'PackagistScrapy (+http://www.yourdomain.com)'
